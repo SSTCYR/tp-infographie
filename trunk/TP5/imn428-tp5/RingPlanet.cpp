105f67c7493bdeca864a234c459d09d581919e6b
FAOuellet <FAOuellet@43c87a20-5934-441b-f7c9-b79e17bd3620>
2012-04-08 13:12:12 +0000

    Added Sun Finished RingPlanet interface Redid the CelestialBody constructor

--- trunk/TP5/imn428-tp5/RingPlanet.cpp
+++ trunk/TP5/imn428-tp5/RingPlanet.cpp
@@ -1,21 +1,27 @@
 #include "RingPlanet.h"
-
+#include "BitmapHandling.h"
 
 RingPlanet::RingPlanet()
 {
-	Construct();
 }
 
+RingPlanet::RingPlanet(float radius, float orbitRadius, float revolution, float rotation, char *planetName)
+{
+	Construct(radius, orbitRadius, revolution, rotation, planetName);
+}
 
 RingPlanet::~RingPlanet()
 {
 }
 
-// TODO : Everything
+// TODO : Draw ring
 void RingPlanet::Draw(Position centerOfRevolution)
 {
+	CelestialBody::Draw(centerOfRevolution);
 }
 
 void RingPlanet::Construct(float radius, float orbitRadius, float revolution, float rotation, char *planetName)
 {
+	LoadBmp(strcat(strcat("Resources/ring_", planetName), ".bmp"), m_Ring);
+	CelestialBody::Construct(radius, orbitRadius, revolution, rotation, planetName);
 }
